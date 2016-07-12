4aad6ffedd3f0d2d65ed901b75fc81526b9d721a
glaux <mirmikh@gmail.com>
2016-07-12 11:50:08 +0300

    ft. add save new train

--- app/controllers/trains_controller.rb
+++ app/controllers/trains_controller.rb
@@ -1,18 +1,34 @@
 class TrainsController < ApplicationController
+  def index
+    @trains = Train.all  
+  end
+
+  def show
+    @train = Train.find(params[:id])  
+  end
+
   def new
+    @train = Train.new
+  end
+
+  def edit
     
   end
 
   def create
-    @train = Train.new(params[:train])
-    # @train.save
+    @train = Train.new(train_params)
+    @train.save
 
     redirect_to @train
   end
 
+  def update
+    
+  end
+
   private
   
-  def user_params
-    params.require(:user).permit(:username, :email, :password, :salt, :encrypted_password)
+  def train_params
+    params.require(:train).permit(:train_type, :coaches_number)
   end
 end
